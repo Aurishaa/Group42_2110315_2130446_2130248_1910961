92241119f20bc859bec439ce376f289b72ed3b96
DankBlackman <jupitergomes7@gmail.com>
2024-04-11 19:15:09 +0600

    a

--- Aurishaa/src/Pharmacist/PharmacistSceneController.java
+++ Aurishaa/src/Pharmacist/PharmacistSceneController.java
@@ -1,12 +1,41 @@
 package Pharmacist;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class PharmacistSceneController implements Initializable {
+
+    @FXML
+    private Label label;
+    @FXML
+    private Button dashboardButton;
+    @FXML
+    private Button leaveRequestButton;
+    @FXML
+    private Button addNewMedicineButton;
+    @FXML
+    private Button checkStockButton;
+    @FXML
+    private Button searchMedicineButton;
+    @FXML
+    private Button distributeMedicineButton;
+    @FXML
+    private Button generateReportButton;
+    @FXML
+    private Button setReminderButton;
+    @FXML
+    private Button researchProposalButton;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -24,7 +53,12 @@ public class PharmacistSceneController implements Initializable {
     }
 
     @FXML
-    private void addNewMedicineOnMouseClick(ActionEvent event){
+    private void addNewMedicineOnMouseClick(ActionEvent event) throws IOException{
+        Parent mainSceneParent = FXMLLoader.load(getClass().getResource("/Pharmacist/AddNewMedicine.fxml"));
+        Scene scene1= new Scene(mainSceneParent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene1);
+        window.show();
     }
 
     @FXML
