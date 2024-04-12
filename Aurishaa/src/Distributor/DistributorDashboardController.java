6e7fe9485650be3389041ba02bb8484088ae95a1
Aurishaa <aurishamehek22@gmail.com>
2024-04-12 15:14:28 +0600

    n

--- Aurishaa/src/Distributor/DistributorDashboardController.java
+++ Aurishaa/src/Distributor/DistributorDashboardController.java
@@ -27,8 +27,14 @@ public class DistributorDashboardController implements Initializable {
     }    
 
     @FXML
-    private void placeOrderOnButtonClick(ActionEvent event) {
+    private void placeOrderOnButtonClick(ActionEvent event) throws IOException {
+        Parent mainSceneParent = FXMLLoader.load(getClass().getResource("/Distributor/PlaceOrder.fxml"));
+        Scene scene1= new Scene(mainSceneParent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene1);
+        window.show();
     }
+    
 
     @FXML
     private void viewInventoryOnButtonClick(ActionEvent event) {
