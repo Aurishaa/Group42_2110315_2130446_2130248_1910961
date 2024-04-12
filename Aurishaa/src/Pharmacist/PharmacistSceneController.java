2b00d6f79a90a0e0dda9509f7aea33989e1e0f05
DankBlackman <jupitergomes7@gmail.com>
2024-04-13 02:19:59 +0600

    a

--- Aurishaa/src/Pharmacist/PharmacistSceneController.java
+++ Aurishaa/src/Pharmacist/PharmacistSceneController.java
@@ -51,11 +51,21 @@ public class PharmacistSceneController implements Initializable {
     }    
 
     @FXML
-    private void dashboardOnMouseClick(ActionEvent event) {
+    private void dashboardOnMouseClick(ActionEvent event) throws IOException {
+        Parent mainSceneParent = FXMLLoader.load(getClass().getResource("/Pharmacist/PharmacistScene.fxml"));
+        Scene scene1= new Scene(mainSceneParent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene1);
+        window.show();   
     }
 
     @FXML
-    private void leaveRequestOnMouseClick(ActionEvent event) {
+    private void leaveRequestOnMouseClick(ActionEvent event) throws IOException {
+        Parent mainSceneParent = FXMLLoader.load(getClass().getResource("/Pharmacist/LeaveRequest.fxml"));
+        Scene scene1= new Scene(mainSceneParent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene1);
+        window.show();         
     }
 
     @FXML
