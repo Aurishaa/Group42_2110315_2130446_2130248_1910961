51c98c86d594a5b47857a2f6282b80dba3a9adea
DankBlackman <jupitergomes7@gmail.com>
2024-04-14 02:58:33 +0600

    a

--- Aurishaa/src/Pharmacist/GenerateReportController.java
+++ Aurishaa/src/Pharmacist/GenerateReportController.java
@@ -13,7 +13,12 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 public class GenerateReportController implements Initializable {
 
@@ -70,5 +75,14 @@ public class GenerateReportController implements Initializable {
     @FXML
     private void clearTableOnMouseClick(ActionEvent event) {
         medData.clear();
+    }
+
+    @FXML
+    private void backButtonOnMouseClick(ActionEvent event) throws IOException {
+        Parent mainSceneParent = FXMLLoader.load(getClass().getResource("/Pharmacist/PharmacistScene.fxml"));
+        Scene scene1= new Scene(mainSceneParent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene1);
+        window.show();        
     }
 }
